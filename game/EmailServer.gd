992d1754bb37b831fa305e9797ffe86fd809d141
MasterProjectLC <dan.inafuku@hotmail.com>
2021-04-25 15:38:21 -0300

    some emails

--- game/EmailServer.gd
+++ game/EmailServer.gd
@@ -19,10 +19,6 @@ onready var Accounts = [
 		"password": NameSystem.COLLEAGUE1[4]
 	},
 	{
-		"email": NameSystem.COLLEAGUE2[3],
-		"password": NameSystem.COLLEAGUE2[4]
-	},
-	{
 		"email": NameSystem.COLLEAGUE4[3],
 		"password": NameSystem.COLLEAGUE4[4]
 	},
@@ -35,24 +31,17 @@ onready var Accounts = [
 		"password": NameSystem.BOSS[4]
 	},
 	{
-		"email": NameSystem.DOCTOR[3],
-		"password": NameSystem.DOCTOR[4]
-	},
-	{
 		"email": NameSystem.TIGUY[3],
 		"password": NameSystem.TIGUY[4]
 	},
 	{
-		"email": NameSystem.CEO[3],
-		"password": NameSystem.CEO[4]
-	},
-	{
-		"email": NameSystem.COMPANY_OWNER[3],
-		"password": NameSystem.COMPANY_OWNER[4]
+		"email": NameSystem.ROGUE[3],
+		"password": NameSystem.ROGUE[4]
 	},
 ]
 
 onready var All_Emails = [
+	# PLAYER ============================================
 	[
 		{
 			"sender": NameSystem.SUPERVISOR[3],
@@ -68,31 +57,31 @@ If you have any questions, reach me on my office line at 555-6160.
 
 Warm regards,
 """ + NameSystem.SUPERVISOR[2] + """, Thaumaturgy Incorporated
-Where your problems disappear""",
-		"annex": "res://icon.png"
-		},
-		{
-			"sender": "JOOOJ",
-			"receivers": ["bgfg", "xcvb"],
-			"date": "dfbdfb",
-			"topic": "gbgb",
-			"body": """wewewewewe"""
+Where your problems disappear"""
 		},
 	],
+	
+	# SUPERVISOR ============================================
+	[
+	],
+	
+	# OWNER ============================================
 	[
 		{
-			"sender": "",
-			"receivers": ["", ""],
-			"topic": "gbgb",
-			"date": "",
-			"body": """""",
-		},
-		{
-			"sender": "",
-			"receivers": ["", ""],
-			"topic": "gbgb",
-			"date": "",
-			"body": """"""
+			"sender": NameSystem.SUPERVISOR[3],
+			"receivers": [NameSystem.OWNER[3]],
+			"date": "03/04/1995",
+			"topic": "Welcome!",
+			"body": "Dear " +  NameSystem.PLAYER[2] + """
+Welcome to the hive!
+It is a genuine pleasure to welcome you to the team. We really hope you enjoy being part of our big family, and we are excited to see what you can do!
+I know you’re probably excited to get started now, but before you do, please check if the computer you received was properly wiped. If not, please run the program “wipe_everything.exe”. DO NOT OPEN ANY FILES PRESENT IN THE COMPUTER.
+As soon as we’re able, we will begin to send assignments for you to complete. As one of our company’s mottos, “time wasted is really bad”, we expect that you don’t miss any of our deadlines!
+If you have any questions, reach me on my office line at 555-6160.
+
+Warm regards,
+""" + NameSystem.SUPERVISOR[2] + """, Thaumaturgy Incorporated
+Where your problems disappear"""
 		},
 	],
 ]
